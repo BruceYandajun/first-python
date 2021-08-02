2e6d51abd90a4b9af53af3232a371f1b8a490a32
Bruce <ydjidil@163.com>
2021-08-02 16:48:44 +0800

    index

--- pandas_test/aggregation/books_group.py
+++ pandas_test/aggregation/books_group.py
@@ -1,7 +1,10 @@
-import pandas as pd
 from pandas import DataFrame
 
 df = DataFrame(data={'books': ['bk1', 'bk1', 'bk1', 'bk2', 'bk2', 'bk3'], 'price': [12, 12, 12, 15, 15, 17]})
 print(df)
-# 将books作为索引
-print(df.groupby('books', as_index=True).sum())
+# 将books作为行索引
+grouped = df.groupby('books', as_index=True).sum()
+print(grouped)
+print(type(grouped))
+print(grouped['price'])
+
