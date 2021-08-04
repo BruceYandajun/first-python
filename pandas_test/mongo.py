d069ec065c035aa2e1da70864d6ab83a5bab2515
Bruce <ydjidil@163.com>
2021-08-04 10:15:47 +0800

    mongo

---
+++ pandas_test/mongo.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import time
+import modin.pandas as mpd
+import ray
+
+
+@ray.remote(num_cpus=4)
+def my_test():
+    df = pd.read_json('/Users/yandajun/Documents/company/项目/浦卡/0720/mongo/mongo.json')
+    pv = df['id'].groupby(df['deviceId']).count().count()
+    print(pv)
+
+
+# my_test.remote()
+
+
+def my_test_modin():
+    mdf = mpd.read_json('/Users/yandajun/Documents/company/项目/浦卡/0720/mongo/mongo.json')
+    pv = mdf['id'].groupby(mdf['deviceId']).count().count()
+    print(pv)
+
+
+# start = time.time()
+# object_id = my_test()
+# print(object_id)
+# end = time.time()
+# print('Runned %d s' % (end - start))
+
+
+start = time.time()
+object_id = my_test.remote()
+print(ray.get(object_id))
+end = time.time()
+print('Runned by modin %d s' % (end - start))
