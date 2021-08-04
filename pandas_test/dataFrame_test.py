b906745dc1bfb60fabe6f46a4ae4ca5c8d795a02
Bruce <ydjidil@163.com>
2021-08-04 11:41:26 +0800

    dataFrame append

--- pandas_test/dataFrame_test.py
+++ pandas_test/dataFrame_test.py
@@ -13,3 +13,4 @@ print()
 data = [{'a': 1, 'b': 2}, {'a': 5, 'b': 10, 'c': 20}]
 df = pd.DataFrame(data)
 print(df)
+
--- pandas_test/json_test.py
+++ pandas_test/json_test.py
@@ -3,4 +3,10 @@ import pandas as pd
 df = pd.read_json('sites.json')
 print(df)
 df.to_csv("sites.csv")
-
+oth = pd.DataFrame({
+    'id': [10, 11],
+    'name': ['a', 'b'],
+    'url': ['http', 'www']
+})
+new_df = df.append(other=oth)
+print(new_df)
