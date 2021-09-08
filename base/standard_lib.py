31f3148a93bd9cb440be60cd1492e58f5bc49ea3
Bruce <ydjidil@163.com>
2021-09-08 14:15:24 +0800

    sys.path[1]

--- base/standard_lib.py
+++ base/standard_lib.py
@@ -2,6 +2,7 @@ import os
 import math
 import random
 from datetime import date
+import sys
 
 print('-------------------')
 print(os.getcwd())
@@ -13,4 +14,7 @@ print(random.choice([3, 1, 2]))
 print(random.randint(0, 9))  # 生成0 ~ 9之间的随机数(包含9)
 
 print(date.today())
+
+print(sys.path[1])
+
 
