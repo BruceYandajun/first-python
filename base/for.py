ef05276f5049fb31cdccb174112c22c6bd593c0b
Bruce <ydjidil@163.com>
2021-09-17 08:56:19 +0800

    for

--- base/for.py
+++ base/for.py
@@ -3,6 +3,11 @@ for i in range(0, 5):
 
 print("---------")
 
-integers = [0 for i in range(5)]
 
+integers = [i * i for i in range(5)]
+print(integers)
+# 以上等价于
+ints = []
+for i in range(5):
+    ints.append(i * i)
 print(integers)
---
+++ base/multiple_returns.py
@@ -0,0 +1,7 @@
+def my_function():
+    return "a", 2, 3
+
+
+a, b, c = my_function()
+
+print(a, b, c)
